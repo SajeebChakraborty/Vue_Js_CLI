9454b7f1af39eb7b02de19ae82df4808e8b80e0f
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-25 00:25:31 +0600

    global filter use

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import Vue from 'vue'
+
+// Vue.config.productionTip = false
+
+// //create global filter
+// Vue.filter('snippet', val=>{
+//     if(!val || typeof(val)!='string') return ''
+//     val = val.slice(0,50)
+//     return val;
+    
+// })
+
 createApp(App).mount('#app')
